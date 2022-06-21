21b3aa7ee973be6b90a5745ffd31c44be1bf6c2d
Isaac Tullos <isaac.tullos@gmail.com>
2022-06-21 14:21:29 -0400

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -14,7 +14,9 @@ module SessionsHelper
   def current_user
     if (user_id = session[:user_id])
       user = User.find_by(id: user_id)
-      @current_user ||= user if session[:session_token] == user.session_token 
+      if user && session[:session_token] == user.session_token
+        @current_user = user
+      end
     elsif (user_id = cookies.encrypted[:user_id])
       user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
@@ -22,6 +24,10 @@ module SessionsHelper
         @current_user = user
       end
     end
+  end
+
+  def current_user?(user)
+    user && user == current_user
   end
 
   def logged_in?
@@ -38,5 +44,9 @@ module SessionsHelper
     forget(current_user)
     reset_session
     @current_user = nil
+  end
+
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
